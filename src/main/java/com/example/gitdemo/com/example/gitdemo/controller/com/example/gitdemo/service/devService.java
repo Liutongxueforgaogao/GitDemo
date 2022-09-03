1cd3ab5e5170df3375a2ab32385574dfd09a2f6d
Liutongxueforgaogao <64537558+Liutongxueforgaogao@users.noreply.github.com>
2022-09-03 15:06:24 +0800

    修复bug

--- src/main/java/com/example/gitdemo/com/example/gitdemo/controller/com/example/gitdemo/service/devService.java
+++ src/main/java/com/example/gitdemo/com/example/gitdemo/controller/com/example/gitdemo/service/devService.java
@@ -1,4 +1,12 @@
 package com.example.gitdemo.com.example.gitdemo.controller.com.example.gitdemo.service;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class devService {
+    @RequestMapping
+    public String m(){
+        return "success";
+    }
 }
